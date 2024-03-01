852ff23f2f0ed07ee8eaecb67f8893b37c79c5f0
Prakhar <agrawalprakhar893@gmail.com>
2024-03-01 18:17:06 +0530

    Display Message using FRC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { environment } from 'src/environments/environment';
+import { getRemoteConfig, fetchAndActivate, getValue } from 'firebase/remote-config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularfrc';
+  greeting: string | undefined;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.initializeApp();
+    this.setupRemoteConfigUpdate();
+  }
+
+  async initializeApp() {
+    // Initialize Firebase
+    const app = initializeApp(environment.firebaseConfig);
+
+    const remoteConfig = getRemoteConfig(app);
+    
+    remoteConfig.settings.minimumFetchIntervalMillis = 0;
+    
+    try {
+      const isFetched = await fetchAndActivate(remoteConfig);
+      if (!isFetched) {
+        const greetingValue = getValue(remoteConfig, 'greeting');
+        this.greeting = greetingValue.asString(); 
+      }
+    } catch (error) {
+      console.error('Error fetching remote config:', error);
+    }
+  }
+
+  setupRemoteConfigUpdate() {
+    setInterval(async () => {
+      try {
+        const remoteConfig = getRemoteConfig();
+        const isFetched = await fetchAndActivate(remoteConfig);
+        if (!isFetched) {
+          const greetingValue = getValue(remoteConfig, 'greeting');
+          this.greeting = greetingValue.asString(); 
+        }
+      } catch (error) {
+        console.error('Error fetching remote config:', error);
+      }
+    }, 1000); //Update Every 1 Sec Interval
+  }
 }
